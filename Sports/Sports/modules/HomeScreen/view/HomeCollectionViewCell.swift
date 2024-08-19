17c4585532c0593f60b04b00c611bb62908adf7d
Marwa Maky <marwamaky50@gmail.com>
2024-08-19 17:32:55 +0300

    Project Update
    Project Update

---
+++ Sports/Sports/modules/HomeScreen/view/HomeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCollectionViewCell.swift
+//  Sporty
+//
+//  Created by marwa maky on 19/08/2024.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var homeImg: UIImageView!
+    @IBOutlet weak var homeLbl: UILabel!
+    
+}
---
+++ Sports/Sports/view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports
+//
+//  Created by marwa maky on 19/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
